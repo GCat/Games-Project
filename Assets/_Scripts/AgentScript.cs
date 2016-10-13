eac1626ff14ba82e472d17fa8f463ed76460f675
Carlos Gonzalez <19charles96@gmail.com>
2016-10-13 20:29:04 +0100

    fixed untracked changes

---
+++ Assets/_Scripts/AgentScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AgentScript : MonoBehaviour {
+	
+	public GameObject[] targets;
+	private NavMeshAgent agent;
+	private int targetnum;
+	private int numTargets;
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent>();
+		targets = GameObject.FindGameObjectsWithTag("Human Waypoint");
+		targetnum = 0;
+		numTargets = targets.Length;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(numTargets != 0){
+			float tx = targets[targetnum].transform.position.x;
+			float tz = targets[targetnum].transform.position.z;
+			if((transform.position.x == tx) && (transform.position.z ==tz)){
+				targetnum = (1 + targetnum) % numTargets;
+			}
+			Vector3 target = targets[targetnum].transform.position;
+			target.y = 0;
+			agent.SetDestination(target);
+		} 
+	}
+}
