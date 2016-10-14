91b476e07bd8b2540927c0354668dbb6a970486e
Carlos Gonzalez <19charles96@gmail.com>
2016-10-14 18:20:43 +0100

    ai wonders around randomly avoiding all obstacles

--- Assets/_Scripts/AgentScript.cs
+++ Assets/_Scripts/AgentScript.cs
@@ -1,31 +1,58 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class AgentScript : MonoBehaviour {
-	
-	public GameObject[] targets;
+
+	public float radius; 
+	private Vector3 initialPosition;
 	private NavMeshAgent agent;
-	private int targetnum;
-	private int numTargets;
-	// Use this for initialization
+
 	void Start () {
+
+		initialPosition = transform.position;
 		agent = GetComponent<NavMeshAgent>();
-		targets = GameObject.FindGameObjectsWithTag("Human Waypoint");
-		targetnum = 0;
-		numTargets = targets.Length;
+		Vector3 target = checkPosition(calculateNewTarget());
+		agent.SetDestination(target);
+		Debug.Log(target);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(numTargets != 0){
-			float tx = targets[targetnum].transform.position.x;
-			float tz = targets[targetnum].transform.position.z;
-			if((transform.position.x == tx) && (transform.position.z ==tz)){
-				targetnum = (1 + targetnum) % numTargets;
+		float remainingDistance = agent.remainingDistance;
+		NavMeshPathStatus pathstatus = agent.pathStatus;
+
+		// Check if we've reached the destination
+		if (!agent.pathPending){
+			if (agent.remainingDistance <= agent.stoppingDistance){
+				if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f){
+					Vector3 target = checkPosition(calculateNewTarget());
+					agent.SetDestination(target);
+					 
+				}
 			}
-			Vector3 target = targets[targetnum].transform.position;
-			target.y = 0;
-			agent.SetDestination(target);
-		} 
+		}
 	}
+
+	Vector3 calculateNewTarget(){
+		/// x ^2 * y^2 <+ r^2
+		/// get random x in range +- radius ^2
+		/// then find y in range 
+		float sqrRadius = radius * radius;
+		float x = Random.Range(-radius, radius);
+		float maxz = Mathf.Sqrt(sqrRadius - x*x);
+		float z = Random.Range(-maxz, maxz);
+		return new Vector3(x + initialPosition.x, 0.5f, z + initialPosition.z);
+	}
+
+	Vector3 checkPosition(Vector3 coords){
+		   Collider[] obstacles = Physics.OverlapSphere(coords, 0.0f);
+		   if (obstacles.Length != 0){
+				Debug.Log("HIT");
+			   Vector3 newTarget = new Vector3 (coords.x + obstacles[0].bounds.size.x, 0.5f, coords.z);
+			   return checkPosition(newTarget);
+		   }
+		   return coords;
+	}
+
 }
