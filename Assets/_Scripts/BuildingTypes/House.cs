54f944c7ad70bc684c176cbdb3c026423d1ba09e
Gavin Parker <Gavin Parker>
2017-01-31 14:58:24 +0000

    snapping and silhouette mostly works now

--- Assets/_Scripts/BuildingTypes/House.cs
+++ Assets/_Scripts/BuildingTypes/House.cs
@@ -98,7 +98,9 @@ public class House  : MonoBehaviour, Building
                 if (transform.position.y > 0.0 && Mathf.Abs(transform.position.x) <= 50 && Mathf.Abs(transform.position.z) <= 100)
                 {
                     highlight.GetComponent<Renderer>().enabled = true;
-                    highlight.transform.position = new Vector3(transform.position.x, 0.1f, transform.position.z);
+                    highlight.transform.position = new Vector3(Mathf.Floor(transform.position.x), 0.1f, Mathf.Floor(transform.position.z));
+                    highlight.transform.rotation = Quaternion.LookRotation(Vector3.forward);
+
                 }
                 else
                 {
@@ -187,7 +189,9 @@ public class House  : MonoBehaviour, Building
         highlight = GameObject.CreatePrimitive(PrimitiveType.Cube);
         highlight.GetComponent<Renderer>().material = mat;
         highlight.transform.localScale = new Vector3(GetComponent<BoxCollider>().bounds.size.x, 0.1f, GetComponent<BoxCollider>().bounds.size.z);
-        highlight.transform.position = new Vector3(transform.position.x, 0.1f, transform.position.z);
+        highlight.transform.position = new Vector3(Mathf.Floor(transform.position.x), 0.1f, Mathf.Floor(transform.position.z));
+        highlight.transform.rotation = Quaternion.LookRotation(Vector3.forward);
+
         highlight.GetComponent<Collider>().enabled = false;
         highlight.GetComponent<Renderer>().enabled = false;
 
@@ -198,9 +202,27 @@ public class House  : MonoBehaviour, Building
     }
     void release(Vector3 vel)
     {
-        GetComponent<Rigidbody>().useGravity = true;
-        GetComponent<Rigidbody>().isKinematic = false;
-        GetComponent<Collider>().enabled = true;
-        GetComponent<Rigidbody>().velocity = vel;
+
+        //Snap to grid
+        float y = transform.position.y;
+        float x = transform.position.x;
+        float z = transform.position.z;
+
+        //test within table bounds
+        if (GameBoard.withinBounds(transform.position))
+        {
+            transform.position = new Vector3(Mathf.Floor(x), 0, Mathf.Floor(z));
+            transform.rotation = Quaternion.LookRotation(Vector3.forward);
+            GetComponent<Rigidbody>().useGravity = false;
+            GetComponent<Rigidbody>().isKinematic = true;
+            GetComponent<Collider>().enabled = true;
+        }
+        else
+        {
+            GetComponent<Rigidbody>().useGravity = true;
+            GetComponent<Rigidbody>().isKinematic = false;
+            GetComponent<Collider>().enabled = true;
+            GetComponent<Rigidbody>().velocity = vel;
+        }
     }
 }
