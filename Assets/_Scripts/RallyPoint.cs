0cc577b02a38caf61488b7a1e3cd5fbd336c294f
awcotuob <ac14808@my.bristol.ac.uk>
2017-03-09 18:56:21 +0000

    pole zone when held human

--- Assets/_Scripts/RallyPoint.cs
+++ Assets/_Scripts/RallyPoint.cs
@@ -7,27 +7,69 @@ public class RallyPoint : Building, Grabbable {
     public string buildingName;
     private int fCost = 0;
 
+    GameObject rangeHighlight;
+    private float radius = 25.0f;
+
     public float timer1;
     private float StartTime1;
+    private float placementTime = 0;
+
 
     bool active = false;
     public bool held = false;
     private bool badplacement = false;
+    bool grabbedOnce = false;
+    bool humanIsHeld = false;
+
 
     //private GameObject infoText;
 
     // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Start() {
+        rangeHighlight = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        rangeHighlight.GetComponent<Renderer>().material.SetColor("_Color", new Color(0.0f, 0.6f, 0.0f, 0.2f));
+        rangeHighlight.transform.localScale = new Vector3(radius, 0.1f, radius);
+        rangeHighlight.transform.position = new Vector3(gameObject.transform.position.x, 0.1f, gameObject.transform.position.z);
+        rangeHighlight.transform.rotation = Quaternion.LookRotation(Vector3.forward);
+
+        rangeHighlight.GetComponent<Collider>().enabled = false;
+        rangeHighlight.GetComponent<Renderer>().enabled = true;
+        rangeHighlight.SetActive(false);
+        InvokeRepeating("HumanHeld", .1f, .1f);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        rangeHighlight.transform.position = new Vector3(gameObject.transform.position.x, 0.1f, gameObject.transform.position.z);
+
+        if (held)
+        {
+            if (highlight != null)
+            {
+                highlightCheck();
+            }
+        }
+        else if (badplacement)
+        {
+            if (Time.time - placementTime > 5.0f)
+            {
+                DestroyObject(gameObject);
+            }
+        }
+        else if (humanIsHeld)
+        {
+            showRange();
+        }
+        else if (!humanIsHeld)
+        {
+            hideRange();
+        }
+    }
 
     public void grab()
     {
+        showRange();
+        grabbedOnce = true;
         held = true;
         badplacement = false;
         //Destroy(infoText);
@@ -86,22 +128,78 @@ public class RallyPoint : Building, Grabbable {
         buildingName = "RALLY";
         timer1 = 0f;
         StartTime1 = Time.time;
+        hideRange();
 
+        
     }
 
-    public override void createHighlight()
-    {
-        highlight = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        highlight.GetComponent<Renderer>().material.SetColor("_Color", Color.green);
-        highlight.transform.localScale = new Vector3(GetComponent<BoxCollider>().bounds.size.x, 0.1f, GetComponent<BoxCollider>().bounds.size.z);
-        highlight.transform.position = new Vector3(Mathf.Floor(transform.position.x), 0.1f, Mathf.Floor(transform.position.z));
-        highlight.transform.rotation = transform.rotation;
 
-        highlight.GetComponent<Collider>().isTrigger = true;
-        highlight.GetComponent<Renderer>().enabled = false;
-    }
     public override void deactivate()
     {
         active = false;
     }
+
+    public void showRange()
+    {
+        rangeHighlight.SetActive(true);
+
+    }
+
+    public void hideRange()
+    {
+        rangeHighlight.SetActive(false);
+
+    }
+
+    new void OnTriggerEnter(Collider other)
+    {
+        base.OnTriggerEnter(other);
+        if (other.gameObject.tag == "Hand" && grabbedOnce)
+        {
+            showRange();
+        }
+    }
+
+    new void release(Vector3 vel)
+    {
+        base.release(vel);
+        hideRange();
+    }
+
+    public void HumanHeld()
+    {
+        GameObject[] humans;
+        humans = GameObject.FindGameObjectsWithTag("Human");
+        foreach (GameObject human in humans)
+        {
+            if (human.GetComponent<Agent>().humanHeld)
+            {
+                humanIsHeld = true;
+                break;
+            }
+            else
+            {
+                humanIsHeld = false;
+            }
+        }
+    }
+
+
 }
+
+/*
+ IEnumerator HumanHeld()
+    {
+        yield return new WaitForFixedUpdate();
+        GameObject[] humans;
+        humans = GameObject.FindGameObjectsWithTag("Human");
+        foreach (GameObject human in humans)
+        {
+            if (human.GetComponent<Agent>().humanHeld)
+            {
+                Debug.Log("IF");
+                humanIsHeld = true;
+            }
+        }
+    }
+ */
