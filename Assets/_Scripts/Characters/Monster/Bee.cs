5c86bc0a929e8758dc521f556cc03f6bfd9698b4
Carlos Gonzalez <19charles96@gmail.com>
2017-04-04 17:49:02 +0100

    Starting bee attack

---
+++ Assets/_Scripts/Characters/Monster/Bee.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bee : MonoBehaviour, HealthManager {
+
+
+    public float damage;
+    public float health;
+    private GameObject target;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Spawn(GameObject target, float timeout)
+    {
+        this.target = target;
+        StartCoroutine(waitToDie(timeout));
+    }
+
+    public void decrementHealth(float damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    IEnumerator waitToDie (float timeout)
+    {
+        yield return new WaitForSeconds(timeout);
+        if (gameObject != null) Destroy(gameObject);
+    }
+}
---
+++ Assets/_Scripts/Events/BeeAttack.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeeAttack : Event {
+
+    /// <summary>
+    /// BEES will attack for a limited amount of time after that all die to symbolize imminent extintion of all bees
+    /// also to avoid events completly recking you.
+    /// BEES will attack temple or humans, yet to be decided
+    /// If target dies bee dies because if a bee strings you you die.
+    /// It will be a simplified version of the badie AI but flying and with no need of the agent hopefully.
+    /// </summary>
+    public override void startEvent(){
+        // Instantiate bees to attack humans and temple? or to attack you
+    }
+}
