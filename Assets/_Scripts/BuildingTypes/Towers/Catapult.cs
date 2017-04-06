c3285a688fe926b72c599f344d23d19d020e26d2
Gavin Parker <gp14958@my.bristol.ac.uk>
2017-04-06 11:50:20 +0100

    Started Catapult

---
+++ Assets/_Scripts/BuildingTypes/Towers/Catapult.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Catapult : Tower {
+    public GameObject pivot;
+    private Quaternion pivotRotation;
+    private Rigidbody pivotRB;
+    // Use this for initialization
+    void Start () {
+        pivotRB = pivot.GetComponent<Rigidbody>();
+        pivotRotation = pivotRB.rotation;
+        InvokeRepeating("fire", 2f, 4f);
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate () {
+        //pivotRB.MoveRotation(pivotRotation);
+    }
+
+    void fire()
+    {
+        Debug.Log("Fire");
+        //pivotRB.MoveRotation(pivotRB.rotation*Quaternion.Euler(0,0,60));
+        pivotRB.AddTorque(new Vector3(0, 0, -500), ForceMode.Impulse);
+    }
+}
