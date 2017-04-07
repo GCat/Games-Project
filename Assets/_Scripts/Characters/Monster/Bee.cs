1490c29c586f396680b872b7f67f16fa100fec3c
Carlos Gonzalez <19charles96@gmail.com>
2017-04-07 12:32:14 +0100

    Bee event

--- Assets/_Scripts/Characters/Monster/Bee.cs
+++ Assets/_Scripts/Characters/Monster/Bee.cs
@@ -7,21 +7,77 @@ public class Bee : MonoBehaviour, HealthManager {
 
     public float damage;
     public float health;
+    public float speed;
+
+    private Vector3 attackPoint = Vector3.zero;
+    private bool active = false;
     private GameObject target;
+    private Rigidbody rb;
+    private float range = 10.0f;
+
 	// Use this for initialization
 	void Start () {
-		
+        rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate () {
+        if (active)
+        {
+            if (target == null) Destroy(gameObject);
+            else
+            {
+                if (attackPoint != Vector3.zero)
+                {
+                    if (Move(attackPoint))
+                    {
+                        StartCoroutine(WaitToAttack(2.0f, target));
+                    }
+                }
+                else
+                {
+                    if (Move(target.GetComponent<Collider>().ClosestPointOnBounds(transform.position)))
+                    {
+                        StartCoroutine(WaitToAttack(2.0f, target));
+                    }
+                }
+            }
+        }
 	}
+
+
+    private bool Move(Vector3 target)
+    {
+        transform.LookAt(target);
+        if (Vector3.Distance(transform.position, target) > range)
+        {
+            Vector3 moveDirection = target - transform.position;
+            Vector3 nMove = moveDirection.normalized;
+            rb.velocity= nMove*speed;
+            Debug.Log(rb.velocity);
+            return false;
+        }
+        else
+        {
+            rb.velocity =Vector3.zero;
+
+            return true;
+        }
+    }
 
     public void Spawn(GameObject target, float timeout)
     {
         this.target = target;
-        StartCoroutine(waitToDie(timeout));
+        active = true;
+    }
+
+    public void Spawn(GameObject target, float timeout, Vector3 t)
+    {
+        this.target = target;
+        attackPoint = t;
+        active = true;
+        transform.LookAt(t);
+
     }
 
     public void decrementHealth(float damage)
@@ -33,9 +89,34 @@ public class Bee : MonoBehaviour, HealthManager {
         }
     }
 
-    IEnumerator waitToDie (float timeout)
+    IEnumerator WaitToDie (float timeout)
     {
         yield return new WaitForSeconds(timeout);
         if (gameObject != null) Destroy(gameObject);
+    }
+
+    IEnumerator WaitToAttack(float timeout, GameObject victim)
+    {
+        yield return new WaitForSeconds(timeout);
+        if( victim != null)
+        {
+            HealthManager hvic = victim.GetComponent<HealthManager>();
+            hvic.decrementHealth(damage);
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+
+    }
+
+    void OnCollisionStay(Collision col)
+    {
+
+    }
+
+    void OnCollisionExit(Collision col)
+    {
+
     }
 }
--- Assets/_Scripts/Events/BeeAttack.cs
+++ Assets/_Scripts/Events/BeeAttack.cs
@@ -11,7 +11,26 @@ public class BeeAttack : Event {
     /// If target dies bee dies because if a bee strings you you die.
     /// It will be a simplified version of the badie AI but flying and with no need of the agent hopefully.
     /// </summary>
+    /// 
+    public int swarmSize;
+    public GameObject beePrefab;
     public override void startEvent(){
-        // Instantiate bees to attack humans and temple? or to attack you
+        GameObject temple = GameObject.FindGameObjectWithTag("Temple");
+        Vector3 tPost = temple.transform.position;
+        Vector3 spawnLocation = GameObject.FindGameObjectWithTag("Portal").transform.position;
+        spawnLocation.y = 30f;
+        spawnLocation.x += Random.Range(-20, 20);
+        spawnLocation.z += Random.Range(-20, 20);
+        if (temple != null)
+        {
+            for (int s= 0; s< swarmSize; s++ )
+            {
+                GameObject bee = GameObject.Instantiate(beePrefab, spawnLocation, Quaternion.identity);
+                Vector3 attackPoint = temple.GetComponent<Collider>().ClosestPointOnBounds(spawnLocation);
+                bee.GetComponent<Bee>().Spawn(temple, 2f, attackPoint);
+                spawnLocation.x += Random.Range(-10, 10);
+                spawnLocation.z += Random.Range(-10, 10);
+            }
+        }
     }
 }
