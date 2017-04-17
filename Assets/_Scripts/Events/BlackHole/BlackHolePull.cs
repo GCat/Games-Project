fa3845777e877fe1f1d9f0312792d2dc3d9e41cb
Carlos Gonzalez <19charles96@gmail.com>
2017-04-17 16:44:55 +0100

    BlackHole stuff

--- Assets/_Scripts/Events/BlackHole/BlackHolePull.cs
+++ Assets/_Scripts/Events/BlackHole/BlackHolePull.cs
@@ -18,45 +18,47 @@ public class BlackHolePull : MonoBehaviour {
     {
         HealthManager thing = other.gameObject.GetComponent<HealthManager>();
         GameObject gameO = other.gameObject;
-        if (other.tag == "Temple")
-        {
-            if (thing != null) thing.decrementHealth(damage * Time.deltaTime);
-        }
-        else if (gameO.GetComponent<Building>() != null)
-        {
-            Vector3 vel = transform.position - gameO.transform.position;
-            vel = vel.normalized;
-            vel = vel * (pullForce / gameO.GetComponent<Rigidbody>().mass);
-            vel.y = 0.0f;
-            gameO.GetComponent<Rigidbody>().isKinematic = false;
-            gameO.GetComponent<Rigidbody>().velocity = vel;
-            if (thing != null) thing.decrementHealth(damage * Time.deltaTime);
-        }
-        else if (gameO.GetComponent<Agent>() != null)
-        {
-            gameO.GetComponent<Agent>().active = false;
-            gameO.GetComponent<Character>().agent.enabled = false;
-            Vector3 vel = transform.position - gameO.transform.position;
-            vel = vel.normalized;
-            vel = vel * (pullForce / gameO.GetComponent<Rigidbody>().mass);
-            gameO.GetComponent<Rigidbody>().isKinematic = false;
-            gameO.GetComponent<Rigidbody>().useGravity = false;
-            gameO.GetComponent<Rigidbody>().velocity = vel;
-            if (thing != null) thing.decrementHealth(damage * Time.deltaTime);
-            gameO.GetComponent<Agent>().debugBlackhole(vel);
+        if (gameO.GetComponent<Rigidbody>() != null){ 
+            if (other.tag == "Temple")
+            {
+                if (thing != null) thing.decrementHealth(damage * Time.deltaTime);
+            }
+            else if (gameO.GetComponent<Building>() != null)
+            {
+                Vector3 vel = transform.position - gameO.transform.position;
+                vel = vel.normalized;
+                vel = vel * (pullForce / gameO.GetComponent<Rigidbody>().mass);
+                vel.y = 0.0f;
+                gameO.GetComponent<Rigidbody>().isKinematic = false;
+                gameO.GetComponent<Rigidbody>().velocity = vel;
+                if (thing != null) thing.decrementHealth(damage * Time.deltaTime);
+            }
+            else if (gameO.GetComponent<Agent>() != null)
+            {
+                gameO.GetComponent<Agent>().active = false;
+                gameO.GetComponent<Character>().agent.enabled = false;
+                Vector3 vel = transform.position - gameO.transform.position;
+                vel = vel.normalized;
+                vel = vel * (pullForce / gameO.GetComponent<Rigidbody>().mass);
+                gameO.GetComponent<Rigidbody>().isKinematic = false;
+                gameO.GetComponent<Rigidbody>().useGravity = false;
+                gameO.GetComponent<Rigidbody>().velocity = vel;
+                if (thing != null) thing.decrementHealth(damage * Time.deltaTime);
+                gameO.GetComponent<Agent>().debugBlackhole(vel);
 
-        }
-        else if (gameO.GetComponent<Monster>() != null)
-        {
-            gameO.GetComponent<Monster>().active = false;
-            gameO.GetComponent<Character>().agent.enabled = false;
-            Vector3 vel = transform.position - gameO.transform.position;
-            vel = vel.normalized;
-            vel = vel * (pullForce / gameO.GetComponent<Rigidbody>().mass);
-            gameO.GetComponent<Rigidbody>().isKinematic = false;
-            gameO.GetComponent<Rigidbody>().useGravity = false;
-            gameO.GetComponent<Rigidbody>().velocity = vel;
-            if (thing != null) thing.decrementHealth(damage * Time.deltaTime);
+            }
+            else if (gameO.GetComponent<Monster>() != null)
+            {
+                gameO.GetComponent<Monster>().active = false;
+                gameO.GetComponent<Character>().agent.enabled = false;
+                Vector3 vel = transform.position - gameO.transform.position;
+                vel = vel.normalized;
+                vel = vel * (pullForce / gameO.GetComponent<Rigidbody>().mass);
+                gameO.GetComponent<Rigidbody>().isKinematic = false;
+                gameO.GetComponent<Rigidbody>().useGravity = false;
+                gameO.GetComponent<Rigidbody>().velocity = vel;
+                if (thing != null) thing.decrementHealth(damage * Time.deltaTime);
+            }
         }
 
     }
@@ -66,38 +68,43 @@ public class BlackHolePull : MonoBehaviour {
         HealthManager thing = other.gameObject.GetComponent<HealthManager>();
         GameObject gameO = other.gameObject;
         if (thing != null) thing.decrementHealth(damage * Time.deltaTime);
-        if (gameO.GetComponent<Building>() != null || gameO.GetComponent<Agent>() != null || gameO.GetComponent<Monster>() != null)
+        if (gameO.GetComponent<Rigidbody>() != null)
         {
-            Vector3 vel = transform.position - gameO.transform.position;
-            vel = vel.normalized;
-            vel = vel * (pullForce / gameO.GetComponent<Rigidbody>().mass);
-            gameO.GetComponent<Rigidbody>().velocity = vel;
+            if (gameO.GetComponent<Building>() != null || gameO.GetComponent<Agent>() != null || gameO.GetComponent<Monster>() != null)
+            {
+                Vector3 vel = transform.position - gameO.transform.position;
+                vel = vel.normalized;
+                vel = vel * (pullForce / gameO.GetComponent<Rigidbody>().mass);
+                gameO.GetComponent<Rigidbody>().velocity = vel;
+            }
         }
-       
     }
 
     private void OnTriggerExit(Collider other)
     {
         GameObject gameO = other.gameObject;
-        if (gameO.GetComponent<Building>() != null)
+        if (gameO.GetComponent<Rigidbody>() != null)
         {
-            gameO.GetComponent<Rigidbody>().isKinematic = true;
-            gameO.GetComponent<Rigidbody>().velocity = Vector3.zero;
-        }
-        else if (gameO.GetComponent<Agent>() != null)
-        {
-            gameO.GetComponent<Agent>().active = true;
-            gameO.GetComponent<Character>().agent.enabled = true;
-            gameO.GetComponent<Rigidbody>().isKinematic = true;
-            gameO.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            if (gameO.GetComponent<Building>() != null)
+            {
+                gameO.GetComponent<Rigidbody>().isKinematic = true;
+                gameO.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            }
+            else if (gameO.GetComponent<Agent>() != null)
+            {
+                gameO.GetComponent<Agent>().active = true;
+                gameO.GetComponent<Character>().agent.enabled = true;
+                gameO.GetComponent<Rigidbody>().isKinematic = true;
+                gameO.GetComponent<Rigidbody>().velocity = Vector3.zero;
 
-        }
-        else if (gameO.GetComponent<Monster>() != null)
-        {
-            gameO.GetComponent<Monster>().active = false;
-            gameO.GetComponent<Character>().agent.enabled = true;
-            gameO.GetComponent<Rigidbody>().isKinematic = true;
-            gameO.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            }
+            else if (gameO.GetComponent<Monster>() != null)
+            {
+                gameO.GetComponent<Monster>().active = false;
+                gameO.GetComponent<Character>().agent.enabled = true;
+                gameO.GetComponent<Rigidbody>().isKinematic = true;
+                gameO.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            }
         }
     }
 }
