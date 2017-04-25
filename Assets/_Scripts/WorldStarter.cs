07050894e39c3d095cc6a09e5d05562022d357a7
ab14188 <ab14188@my.bristol.ac.uk>
2017-04-25 12:47:13 +0100

    text added to final image

--- Assets/_Scripts/WorldStarter.cs
+++ Assets/_Scripts/WorldStarter.cs
@@ -5,6 +5,8 @@ using System.IO;
 
 public class WorldStarter : MonoBehaviour {
     public ColorSourceManager colorManager;
+    private float startTime;
+    private Rect TextAreaRect = new Rect( 25, 25, 250, 100);
 
     void Start () {
         
@@ -16,8 +18,7 @@ public class WorldStarter : MonoBehaviour {
 	}
 
 	public void startGame(){
-
-
+        startTime = Time.time;
     }
 
     public void stopGame(){
@@ -29,12 +30,28 @@ public class WorldStarter : MonoBehaviour {
         }
     }
 
+
     private void takeMugShot()
     {
-        Texture2D mugshot = colorManager.GetColorTexture();
+        Texture2D mugshot = colorManager.GetColorTexture();  
         byte[] pixels = mugshot.EncodeToPNG();
+        string text = addtimeScore();
+        byte[] score = System.Text.Encoding.ASCII.GetBytes(text);
+        byte[] final = new byte[pixels.Length + score.Length];
+        System.Buffer.BlockCopy(pixels, 0, final, 0, pixels.Length);
+        System.Buffer.BlockCopy(score, 0, final, pixels.Length, score.Length);
         string id = System.Guid.NewGuid().ToString("N");
-        File.WriteAllBytes("images/" + id + ".png", pixels);
+        File.WriteAllBytes("images/" + id + ".png", score); 
+    }
 
+
+
+    private string addtimeScore()
+    {
+        float now = Time.time - startTime;
+        float min = Mathf.Floor(now / 60);
+        float sec = now % 60;
+        string text = "Lasted " + min + " min" + sec +  " s!";
+        return text;
     }
 }
