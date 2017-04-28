2ee5445037c34899abfefcdcd8f29e63e9c9a5fd
Gavin Parker <gp14958@my.bristol.ac.uk>
2017-04-28 17:16:12 +0100

    Added hades as final boss

---
+++ Assets/_Scripts/Characters/Hades.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hades : MonoBehaviour {
+
+    public Animator animator;
+    Material original;
+    public Material stoneMat;
+    public GameObject playerHead;
+
+    public GameObject r_hand;
+    public GameObject l_hand;
+    public GameObject head;
+    public GameObject chest;
+
+
+    enum BossState {Idle, Ranged, Melee, Dead };
+    BossState state = BossState.Idle;
+	// Use this for initialization
+	void Start () {
+        foreach (Renderer renderer in GetComponentsInChildren<Renderer>())
+        {
+            renderer.material = stoneMat;
+
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    	
+	}
+
+
+    IEnumerator attack()
+    {
+        while (state != BossState.Dead)
+        {
+            switch (state)
+            {
+                case BossState.Idle:
+                    break;
+                case BossState.Ranged:
+                    rangedAttack();
+                    break;
+                case BossState.Melee:
+                    break;
+                case BossState.Dead:
+                    break;
+            }
+        }
+        yield return new WaitForSeconds(5f);
+
+    }
+
+
+    void rangedAttack()
+    {
+        animator.SetTrigger("Fire");
+    }
+
+    void meleeAttack()
+    {
+        animator.SetBool("Punch", true);
+    }
+
+    void fire()
+    {
+
+
+    }
+
+    public void comeAlive()
+    {
+        foreach (Renderer renderer in GetComponentsInChildren<Renderer>())
+        {
+            renderer.material = original;
+        }
+        state = BossState.Ranged;
+        animator.SetBool("Celebrate", true);
+    }
+
+
+
+    public void celebrate()
+    {
+        //animator.SetBool("Celebrate", true);
+
+    }
+}
---
+++ Assets/_Scripts/Events/FinalBoss.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalBoss : WaveEvent
+{
+
+    /// <summary>
+    /// BEES will attack for a limited amount of time after that all die to symbolize imminent extintion of all bees
+    /// also to avoid events completly recking you.
+    /// BEES will attack temple or humans, yet to be decided
+    /// If target dies bee dies because if a bee strings you you die.
+    /// It will be a simplified version of the badie AI but flying and with no need of the agent hopefully.
+    /// </summary>
+    /// 
+    Hades hades;
+    public override void startEvent()
+    {
+        hades = GameObject.FindGameObjectWithTag("Hades").GetComponent<Hades>();
+        hades.comeAlive();
+    }
+}
